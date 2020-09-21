1d41cf66e18df010bf0377740047eaff5a0fffb1
yogi samuel purba <yogipurba87@gmail.com>
2020-09-21 14:57:31 +0700

    Test2.java

---
+++ TP03/Test2.java
@@ -0,0 +1,8 @@
+public class Test2{
+ public static void main(String[]args){
+ int umur = 20;
+ String name ="Yogi";
+ System.out.println("Halo, " + name);
+ System.out.println("Ternyata usiamu baru "+ umur + " tahun ya");
+ }
+}
